cec1f8f00381054fd12d46130f1a79c8da6a7804
cvicens <carlos.vicens.alonso@gmail.com>
2020-03-17 12:25:52 +0100

    Revert "interim"
    This reverts commit eab574bd091ca4b0e46c7d07c012399a291b693d.

--- events/src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Event(id, name, address, city, province, country, date, start_time, end_time, location, artist, description, image) 
-VALUES ( nextval ('hibernate_sequence') , 'Not in the Lifetime Tour', 'Cmo. de Perales, 23, 28041', 'MADRID', 'MADRID', 'SPAIN', '2018-08-05', '18:00', '23:00', 'Caja Magica', 'Guns n Roses', 'The revived Guns N’ Roses and ...', 'guns-P1080795.jpg' );
-INSERT INTO Event(id, name, address, city, province, country, date, start_time, end_time, location, artist, description, image) 
-VALUES ( nextval ('hibernate_sequence') , 'CONCRETE AND GOLD TOUR 2018', '8 Boulevard de Bercy, Paris', 'PARIS', 'PARIS', 'FRANCE', '2018-09-15', '18:00', '23:00', 'AccorHotels Arena', 'Foo Fighters', 'Concrete and Gold Tour is...', 'foo-P1000628.jpg' );
--- events/src/test/java/com/redhat/gramola/events/NativeEventResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.redhat.gramola.events;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeEventResourceIT extends EventResourceTest {
-
-    // Execute the same tests but in native mode.
-}
